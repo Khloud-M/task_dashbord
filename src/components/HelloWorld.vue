79d060386e139be38b8cba516e0648f47e363a28
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-09-20 04:37:47 +0300

    updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,18 @@
 <template>
   <v-container class="fill-height">
-    <h2> أهلا بك  </h2>
+    <h2>أهلا بك</h2>
   </v-container>
 </template>
 
 <script setup>
-  //
+//
 </script>
 <style scoped>
-h2{
+h2 {
   text-align: center;
-  color:var(--main-color);
+  color: var(--main-color);
   /* display: block; */
   margin: auto;
   font-size: 35px;
-
 }
 </style>
